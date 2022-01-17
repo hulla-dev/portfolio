f988c9786c07354329d699a8698c1973b5ed8455
samuelhulla <oficialny.hulla@gmail.com>
2022-01-17 22:24:17 +0100

    :zap: Feat: Tailwind installation

--- pages/index.server.tsx
+++ pages/index.server.tsx
@@ -2,9 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <h1 className="text-3xl font-bold underline">
       Hello world
-    </div>
+    </h1>
   )
 }
 

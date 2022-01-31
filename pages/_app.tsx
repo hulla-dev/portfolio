8e54343e3b16b46ed1246fff968c33f166809429
samuelhulla <oficialny.hulla@gmail.com>
2022-02-01 00:49:08 +0100

    :recycle: Cleanup: Uniform const function declaration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+)
 
 export default App

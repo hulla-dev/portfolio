5d27ff50f87291be5eb2c06d2a5c20e429f6fcfe
samuelhulla <oficialny.hulla@gmail.com>
2022-01-31 20:05:14 +0100

    :recycle: Cleanup: Parallax reduced window subscriptions + console cleanup

--- components/Parallax.client.tsx
+++ components/Parallax.client.tsx
@@ -18,36 +18,21 @@ type Props = {
   alt: string,
   scrollY: MotionValue<number>,
   customStyle?: Object,
-  bg?: boolean
+  bg?: boolean,
+  screenWidth: number,
 }
 
+/* ------------------------ Animated Component Layer ------------------------ */
 const AnimatedLayer = (props: Props) => {
-  const [width, setWidth] = useState(0)
-  
-  useEffect(() => {
-    // We need to check for window, because it is not available on initial SSR
-    if (window && typeof window !== undefined) {
-      const onResizeChange = () => {
-        setWidth(window.innerWidth)
-      }
-      // We call it once to initially get the screen size
-      onResizeChange()
-      // And we add a resize listener in case the user ever swaps screens
-      window.addEventListener('resize', onResizeChange)
-      // And we can't forget to unsubscribe from event listener on unmount
-      return window.removeEventListener('resizeWatcher', onResizeChange)
-    }
-  }, [])
-
   const {
     speed,
     src,
     alt,
     scrollY,
     bg = false,
+    screenWidth,
     customStyle = {},
   } = props
-  
 
   const inputRange = [0, 400, 500, 700]
   const outputRange = bg ? inputRange : [0, 400, 550, 600]
@@ -61,15 +46,13 @@ const AnimatedLayer = (props: Props) => {
 
   const style = { ...customStyle, y }
   
-  if (width >= 2560) {
+  if (screenWidth >= 2560) {
     return (
       <figure className="w-full absolute z-1">
         <Image layout="responsive" src={src} alt={alt} />
       </figure>
     )
   }
-
-  console.log('width', width)
 
   return (
     <motion.figure style={style} className="w-full absolute z-1">
@@ -79,20 +62,38 @@ const AnimatedLayer = (props: Props) => {
   )
 }
 
+/* ---- Complete Parallax section consisting of multiple animated layers ---- */
 const Parallax = () => {
   const { scrollY } = useViewportScroll()
+  const [screenWidth, setScreenWidth] = useState(0)
+  
+  useEffect(() => {
+    // We need to check for window, because it is not available on initial SSR
+    if (window && typeof window !== undefined) {
+      const onResizeChange = () => {
+        setScreenWidth(window.innerWidth)
+      }
+      // We call it once to initially get the screen size
+      onResizeChange()
+      // And we add a resize listener in case the user ever swaps screens
+      window.addEventListener('resize', onResizeChange)
+      // And we can't forget to unsubscribe from event listener on unmount
+      return window.removeEventListener('resizeWatcher', onResizeChange)
+    }
+  }, [])
+
   return (
-    <header id="parallax" className="overflow-y-hidden relative block w-full z-10 h-[400px] md:h-[750px] lg:h-[900px] xl:h-[1280px] uhd:h-[1900px] qhd:h-[1900px]">
-      <AnimatedLayer scrollY={scrollY} src={background} bg alt="background" speed={1} />
-      <AnimatedLayer scrollY={scrollY} src={stars} alt="stars" speed={0.8} />
-      <AnimatedLayer scrollY={scrollY} src={moon} alt="moon" speed={1.5} />
-      <AnimatedLayer scrollY={scrollY} src={clouds} alt="clouds" speed={0.9} />
-      <AnimatedLayer scrollY={scrollY} src={surfaceLevel4} alt="surfaceLevel4" speed={1} />
-      <AnimatedLayer scrollY={scrollY} src={surfaceLevel3} alt="surfaceLevel3" speed={0.9} />
-      <AnimatedLayer scrollY={scrollY} src={surfaceLevel2} alt="surfaceLevel2" speed={0.6} />
-      <AnimatedLayer scrollY={scrollY} src={surfaceLevel1} alt="surfaceLevel1" speed={0.3} />
-      <AnimatedLayer scrollY={scrollY} src={surfaceLevel0} alt="surfaceLevel0" speed={0.2} />
-    </header>
+    <section id="parallax" className="overflow-y-hidden relative block w-full z-10 h-[400px] md:h-[750px] lg:h-[900px] xl:h-[1280px] uhd:h-[1900px] qhd:h-[1900px]">
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={background} bg alt="background" speed={1} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={stars} alt="stars" speed={0.8} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={moon} alt="moon" speed={1.5} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={clouds} alt="clouds" speed={0.9} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={surfaceLevel4} alt="surfaceLevel4" speed={1} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={surfaceLevel3} alt="surfaceLevel3" speed={0.9} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={surfaceLevel2} alt="surfaceLevel2" speed={0.6} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={surfaceLevel1} alt="surfaceLevel1" speed={0.3} />
+      <AnimatedLayer screenWidth={screenWidth} scrollY={scrollY} src={surfaceLevel0} alt="surfaceLevel0" speed={0.2} />
+    </section>
   )
 }
 

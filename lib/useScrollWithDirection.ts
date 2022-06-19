bf7b086555da4f5c4a9a5574ec5d47c91a96e4e7
samuelhulla <oficialny.hulla@gmail.com>
2022-06-19 21:21:40 +0200

    :wrench: Update: QoL improvements to useScrollWithDirection

--- lib/useScrollWithDirection.ts
+++ lib/useScrollWithDirection.ts
@@ -1,14 +1,14 @@
-import { ScrollMotionValues, useViewportScroll } from "framer-motion"
-import { useEffect, useState } from "react"
+import { ScrollMotionValues, useViewportScroll } from 'framer-motion'
+import { useEffect, useState } from 'react'
 
 type ScrollDirection = 'up' | 'down'
 type ScrollState = {
-  scrollDirection: ScrollDirection,
-  scrollY: number, // used to internally track values
+  scrollDirection: ScrollDirection
+  scrollY: number // used to internally track values
 }
 
 type AnimatedScrollState = {
-  scrollDirection: ScrollDirection,
+  scrollDirection: ScrollDirection
 } & ScrollMotionValues
 
 /**
@@ -16,11 +16,13 @@ type AnimatedScrollState = {
  * @param defaultValue Default scroll direction (SSR), defaults to `'down'`
  * useful for defining default behavior in animations
  * @warn It's still advisable to use default `useViewPortScroll` from motion when possible
- * as it is less expensive than this hook. 
- * 
+ * as it is less expensive than this hook.
+ *
  * **This should only be used when you need to update scrollDirection**
  */
-export const useScrollWithDirection = (defaultValue: ScrollDirection = 'down'): AnimatedScrollState => {
+export const useScrollWithDirection = (
+  defaultValue: ScrollDirection = 'down',
+): AnimatedScrollState => {
   const motionScroll = useViewportScroll()
   const [scrollControl, setScrollControl] = useState<ScrollState>({
     scrollY: motionScroll.scrollYProgress.get(),
@@ -32,10 +34,10 @@ export const useScrollWithDirection = (defaultValue: ScrollDirection = 'down'): 
     const updateScrollDirection = () => {
       // Note - no eventListener is necessary as motion ships it's own subscription
       const newScrollY = motionScroll.scrollYProgress.get()
-      setScrollControl(
-        ({ scrollY: prevScrollY }) => newScrollY > prevScrollY
-          ? { scrollY: newScrollY, scrollDirection: 'down', }
-          : { scrollY: newScrollY, scrollDirection: 'up', }
+      setScrollControl(({ scrollY: prevScrollY }) =>
+        newScrollY > prevScrollY
+          ? { scrollY: newScrollY, scrollDirection: 'down' }
+          : { scrollY: newScrollY, scrollDirection: 'up' },
       )
     }
     return motionScroll.scrollYProgress.onChange(updateScrollDirection)
@@ -43,7 +45,7 @@ export const useScrollWithDirection = (defaultValue: ScrollDirection = 'down'): 
 
   return {
     ...motionScroll,
-    scrollDirection: scrollControl.scrollDirection
+    scrollDirection: scrollControl.scrollDirection,
   }
 }
 

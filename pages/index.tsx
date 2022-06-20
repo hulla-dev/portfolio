8ce2bcddb96514a19d0161e1c2c0d434af99901f
samuelhulla <oficialny.hulla@gmail.com>
2022-06-20 20:48:20 +0200

    :zap: Feat: Base Page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
 import Parallax from '../components/Parallax.client'
 import Page from '../components/Page.server'
+import Content from '../components/Content.server'
 
 const Home: NextPage = () => {
   return (
     <div className="h-[4000px]">
       <Page title="Welcome to my Portfolio" header={<Parallax />}>
-        <p>example</p>
+        <Content>
+          <h1>Hi, I'm Samuel Hulla.</h1>
+          <h3>Welcome to my portfolio</h3>
+        </Content>
       </Page>
     </div>
   )

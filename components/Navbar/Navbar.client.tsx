4bf760f6fa749f1d4b1495c16a50c95f19b260d5
samuelhulla <oficialny.hulla@gmail.com>
2022-07-14 18:06:33 +0200

    :wrench: Update: Slightly better navbar smoothness

--- components/Navbar/Navbar.client.tsx
+++ components/Navbar/Navbar.client.tsx
@@ -25,9 +25,9 @@ const Navbar = () => {
       height: 0,
       opacity: 0,
       transition: {
-        delay: 0.5,
+        delay: 0.4,
         type: 'spring',
-        duration: 0.5,
+        duration: 0.4,
       },
     },
   }
